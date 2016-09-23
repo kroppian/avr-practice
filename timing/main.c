9783b9dae4600886c3a7d9b9424c9e1d24ea8201
Ian Kropp <iankropp@gmail.com>
2016-09-22 23:56:08 -0400

    Made a replica of an LED head lamp!

--- timing/main.c
+++ timing/main.c
@@ -20,13 +20,33 @@ int main(void)
   // Turn the input to high voltage 
   PORTB |= (1 << PINB1);
 
-  _delay_ms(500);
+  TCCR1B |= (1 <<  CS10 | 1 <<  CS11);
+
+  short short_goal = 2232;
+  short long_goal = short_goal * 2;
+  short goal = long_goal;
+  short off = -1;
 
   while(1){
- 
-    if(clicked(PINB,PINB1,0))   
-      PORTB ^= (1 << PINB0);
-  
+
+    if(clicked(PINB,PINB1,0)){
+      if(goal != -1 && goal < long_goal)
+        goal = off;
+      else if (goal != -1 )
+        goal = short_goal;
+      else 
+        goal = long_goal;
+    }
+
+    if(goal == -1)
+       PORTB &= ~(1 << PINB0);
+    else if(TCNT1 > goal){
+   
+       TCNT1 = 0;
+       PORTB ^= (1 << PINB0);
+    
+    }
+
   }
 
   // 11111011
