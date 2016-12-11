d26fe66d0a5c8cbb23f877f83930527b3d046b24
Ian Kropp <iankropp@gmail.com>
2016-12-11 16:43:05 -0500

    Made a simple adding counter with the rot encod and lcd

--- rotaryEncoder/main.c
+++ rotaryEncoder/main.c
@@ -1,17 +1,50 @@
 
-#include "lcdlib.h"
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+
+#define BTN_COUNT 2
+#define CONFIDENCE 1
+
+
+#include "liblcd.h"
+#include "libbtn.h"
 
 
 int main(void)
 {
 
+  DDRC |= 1 << PINC2;
+  DDRC &= (1 << PINC1);
+  DDRC &= (1 << PINC0);
+  
+  int myNum = 0;
 
   initialize_lcd();
 
-  int i;
+  send_a_string("Hello again, Ian");
+  go_to_lcd_position(0,1);
+  send_a_num(myNum);
+
+  while(1){
+
+    // if the thing is turning 
+    if(pressed(PINC,PINC1,0)){
+    
+      myNum++; 
+      // Flip the light
+      PORTC ^= 1 << PINC2;
+
+      // increase the number
+      go_to_lcd_position(0,1);
+      send_a_num(myNum);
+
+    }
+
+
+  }
+
+  /*int i;
   while(1){
 
 
@@ -28,7 +61,9 @@ int main(void)
     
     }
         
-  }
+  }*/
+
+  
 
 }
 
