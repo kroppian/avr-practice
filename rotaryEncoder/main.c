e27bfc1b24b64b3e92b0cb471edc7192e632f71e
Ian Kropp <iankropp@gmail.com>
2016-12-17 17:34:21 -0500

    Making the main program a bit prettier

--- rotaryEncoder/main.c
+++ rotaryEncoder/main.c
@@ -11,23 +11,30 @@ int main(void)
 
   DDRC |= 1 << PINC2;
   
-  int myNum = 0;
+  int myNum = 50;
 
   initialize_lcd();
 
   initialize_rot(DDRC, PORTC, PINC1, DDRC, PORTC, PINC0);
 
-  send_a_string("Hello Daniel!");
+  send_a_string("Turn to + or -!");
+
+  int is_rotating = 0;
 
   while(1){
 
-
-    if(rotating() == LEFT){
+    if(! is_rotating && rotating() == LEFT){
       go_to_lcd_position(0,1);
-      send_a_string("left ");
-    }else if(rotating() == RIGHT){
+      myNum--;
+      send_a_num(myNum);
+      is_rotating = 1;
+    }else if(! is_rotating && rotating() == RIGHT){
       go_to_lcd_position(0,1);
-      send_a_string("right");
+      myNum++;
+      is_rotating = 1;
+      send_a_num(myNum);
+    }else if(rotating() == NO_ROT) {
+      is_rotating = 0; 
     }
 
   }
